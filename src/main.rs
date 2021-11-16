05764a52dbc9df0a151dc70605c64942014eabbc
maxwellherron5 <maxwellherron5@gmail.com>
2021-11-16 13:14:41 -0600

    removed token ...

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,6 @@ use std::env;
 use rpi_led_matrix::{LedMatrix, LedMatrixOptions, LedColor};
 use std::{thread, time};
 
-// fn main() {
-//     loop {
-//         draw_line();
-//         thread::sleep(time::Duration::from_millis(5000));
-//         draw_circle();
-//         println!("Hello, world!");
-//         thread::sleep(time::Duration::from_millis(5000));
-//     }
-// }
-
 fn led_matrix() -> LedMatrix {
     let mut options = LedMatrixOptions::new();
     // options.set_hardware_mapping("adafruit-hat-pwm");
@@ -97,13 +87,11 @@ impl EventHandler for Handler {
         println!("{} is connected!", ready.user.name);
     }
 }
-// DISCORD_TOKEN=ODEyMTA1NzcwMzYzMzg3OTc3.YC76bw.UL_A3W99AXVDDANz04FfvOlc9_A
 
 
 #[tokio::main]
 async fn main() {
-    // let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    let token = "ODEyMTA1NzcwMzYzMzg3OTc3.YC76bw.UL_A3W99AXVDDANz04FfvOlc9_A";
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let mut client = Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
 
     if let Err(why) = client.start().await {
